d455fd31ed3972d5a5f85f8a43d43c656afa0ab0
Bill Shaw <majbill@yahoo.com>
2026-07-02 14:16:57 -0700

    forgot this one

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'Quiz_GUI_QT.ui'
+## Form generated from reading UI file 'quiz_gui_qt.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.11.1
 ##
@@ -15,7 +15,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QMainWindow,
+from PySide6.QtWidgets import (QApplication, QButtonGroup, QLabel, QMainWindow,
     QMenuBar, QPushButton, QRadioButton, QSizePolicy,
     QStatusBar, QWidget)
 
@@ -23,42 +23,45 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(467, 388)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.lblQuestion = QLabel(self.centralwidget)
         self.lblQuestion.setObjectName(u"lblQuestion")
-        self.lblQuestion.setGeometry(QRect(50, 90, 521, 61))
+        self.lblQuestion.setGeometry(QRect(30, 20, 401, 61))
         self.lblQuestion.setTextFormat(Qt.TextFormat.RichText)
         self.lblQuestion.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
         self.btnBack = QPushButton(self.centralwidget)
         self.btnBack.setObjectName(u"btnBack")
-        self.btnBack.setGeometry(QRect(40, 350, 81, 26))
+        self.btnBack.setGeometry(QRect(30, 230, 81, 26))
         self.btnNext = QPushButton(self.centralwidget)
         self.btnNext.setObjectName(u"btnNext")
-        self.btnNext.setGeometry(QRect(150, 350, 81, 26))
+        self.btnNext.setGeometry(QRect(140, 230, 81, 26))
         self.btnQuit = QPushButton(self.centralwidget)
         self.btnQuit.setObjectName(u"btnQuit")
-        self.btnQuit.setGeometry(QRect(40, 410, 81, 26))
-        self.grpRadio = QGroupBox(self.centralwidget)
-        self.grpRadio.setObjectName(u"grpRadio")
-        self.grpRadio.setGeometry(QRect(40, 160, 501, 151))
-        self.radioButton_2 = QRadioButton(self.grpRadio)
+        self.btnQuit.setGeometry(QRect(30, 290, 81, 26))
+        self.radioButton_2 = QRadioButton(self.centralwidget)
+        self.buttonGroup_1 = QButtonGroup(MainWindow)
+        self.buttonGroup_1.setObjectName(u"buttonGroup_1")
+        self.buttonGroup_1.addButton(self.radioButton_2)
         self.radioButton_2.setObjectName(u"radioButton_2")
-        self.radioButton_2.setGeometry(QRect(20, 50, 98, 24))
-        self.radioButton_4 = QRadioButton(self.grpRadio)
+        self.radioButton_2.setGeometry(QRect(30, 110, 98, 24))
+        self.radioButton_4 = QRadioButton(self.centralwidget)
+        self.buttonGroup_1.addButton(self.radioButton_4)
         self.radioButton_4.setObjectName(u"radioButton_4")
-        self.radioButton_4.setGeometry(QRect(20, 110, 98, 24))
-        self.radioButton = QRadioButton(self.grpRadio)
-        self.radioButton.setObjectName(u"radioButton")
-        self.radioButton.setGeometry(QRect(20, 20, 98, 24))
-        self.radioButton_3 = QRadioButton(self.grpRadio)
+        self.radioButton_4.setGeometry(QRect(30, 170, 98, 24))
+        self.radioButton_1 = QRadioButton(self.centralwidget)
+        self.buttonGroup_1.addButton(self.radioButton_1)
+        self.radioButton_1.setObjectName(u"radioButton_1")
+        self.radioButton_1.setGeometry(QRect(30, 80, 98, 24))
+        self.radioButton_3 = QRadioButton(self.centralwidget)
+        self.buttonGroup_1.addButton(self.radioButton_3)
         self.radioButton_3.setObjectName(u"radioButton_3")
-        self.radioButton_3.setGeometry(QRect(20, 80, 98, 24))
+        self.radioButton_3.setGeometry(QRect(30, 140, 98, 24))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.menubar.setGeometry(QRect(0, 0, 467, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -71,14 +74,13 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.lblQuestion.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.lblQuestion.setText("")
         self.btnBack.setText(QCoreApplication.translate("MainWindow", u"Back", None))
         self.btnNext.setText(QCoreApplication.translate("MainWindow", u"Next", None))
         self.btnQuit.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
-        self.grpRadio.setTitle("")
-        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
-        self.radioButton_4.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
-        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
-        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
+        self.radioButton_2.setText("")
+        self.radioButton_4.setText("")
+        self.radioButton_1.setText("")
+        self.radioButton_3.setText("")
     # retranslateUi
 
